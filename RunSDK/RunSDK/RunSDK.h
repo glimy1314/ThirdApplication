d18c9c46bfed0ab139a1202bb9b824718333f795
guoliang <guoliang@baiing.cn>
2020-11-05 13:53:59 +0800

    Release 1.0.0

---
+++ RunSDK/RunSDK/RunSDK.h
@@ -0,0 +1,18 @@
+//
+//  RunSDK.h
+//  RunSDK
+//
+//  Created by Bright on 2020/11/4.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RunSDK.
+FOUNDATION_EXPORT double RunSDKVersionNumber;
+
+//! Project version string for RunSDK.
+FOUNDATION_EXPORT const unsigned char RunSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RunSDK/PublicHeader.h>
+
+
---
+++ RunSDK/RunSDK/RunSDK.swift
@@ -0,0 +1,14 @@
+//
+//  RunSDK.swift
+//  RunSDK
+//
+//  Created by Bright on 2020/11/4.
+//
+
+import Foundation
+
+class RunTool {
+    func startRun() {
+        print("startRun")
+    }
+}
